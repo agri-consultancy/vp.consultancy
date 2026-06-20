a9ed145b76c4c606b5dc41dbb58d160cf57a5b79
Shidhanath Chavan <shidhanath.chavan@gupshup.io>
2026-06-20 11:35:39 +0530

    Initial commit

---
+++ src/main/java/com/example/vp/consultancy/Application.java
@@ -0,0 +1,13 @@
+package com.example.vp.consultancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
